feb454171194e083ce915f947bd30936c54fb349
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-05-23 19:40:37 +0530

    Create 22.cpp

---
+++ 22.cpp
@@ -0,0 +1,49 @@
+// C++ program to find smallest and
+// second smallest elements
+#include <bits/stdc++.h>
+using namespace std; /* For INT_MAX */
+
+void print2Smallest(int arr[], int arr_size)
+{
+	int i, first, second;
+
+	/* There should be atleast two elements */
+	if (arr_size < 2)
+	{
+		cout<<" Invalid Input ";
+		return;
+	}
+
+	first = second = INT_MAX;
+	for (i = 0; i < arr_size ; i ++)
+	{
+		/* If current element is smaller than first
+		then update both first and second */
+		if (arr[i] < first)
+		{
+			second = first;
+			first = arr[i];
+		}
+
+		/* If arr[i] is in between first and second
+		then update second */
+		else if (arr[i] < second && arr[i] != first)
+			second = arr[i];
+	}
+	if (second == INT_MAX)
+		cout << "There is no second smallest element\n";
+	else
+		cout << "The smallest element is " << first << " and second "
+			"Smallest element is " << second << endl;
+}
+
+/* Driver code */
+int main()
+{
+	int arr[] = {12, 13, 1, 10, 34, 1};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	print2Smallest(arr, n);
+	return 0;
+}
+
+
